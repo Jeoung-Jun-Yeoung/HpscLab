f7aacfeeb1fcdbbe089db6afd1cdec82830775f4
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2022-01-20 14:39:09 +0900

    add : cuda code

--- Cuda_temp/Cuda_temp/kernel.cu
+++ Cuda_temp/Cuda_temp/kernel.cu
@@ -1,17 +1,23 @@
 #include "kernel.cuh"
+
+
+__host__ __device__
 double getValue(int M, int N, int x_row, int y_col, double* List) {
 	int Ind = x_row * N + y_col;
 	return List[Ind];
 }
 
+__host__ __device__
 int getRowInd(int M, int N, int Ind) {
 	return (int)(Ind / N);
 }
 
+__host__ __device__
 int getColInd(int M, int N, int Ind) {
 	return (int)(Ind % N);
 }
 
+__host__ __device__
 void getMulti(int M, int N, int K, int ind, double* A, double* B, double* C) {
 	C[ind] = 0.;
 	int x_row = getRowInd(M, K, ind);
@@ -25,12 +31,18 @@ void getMulti(int M, int N, int K, int ind, double* A, double* B, double* C) {
 	}
 }
 
+__global__
 void Kernel(int M, int N, int K, double* A, double* B, double* C) {
 
-	int ind = 0;
-	omp_set_num_threads(2);
-#pragma omp parallel for private(ind)
-	for (ind = 0; ind < M * K; ind++) {
-		getMulti(M, N, K, ind, A, B, C);
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+	// blockDim.x = 블럭이 갖고 있는 쓰레드 숫자.
+	// blockIdx.x = 쓰레드가 몇번째 블럭에 속하는지.
+	// 곱한값은 블럭의 첫번째 쓰레드가 총 쓰레드중 몇번째 스레드인지 구하고, 블럭안에서 몇번째인지 더해주면 발생된 쓰레드중 몇번째 쓰레드인지 알 수 있다.
+
+	// gpu에서는 32, 즉 sm단위로 쓰레드 생성
+
+	if(id < M*K) { 
+		getMulti(M, N, K, id, A, B, C);
 	}
+	// 고유의 주소값으로 각각 다른 동작을 하게 된다.
 }
--- Cuda_temp/Cuda_temp/main.cu
+++ Cuda_temp/Cuda_temp/main.cu
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "kernel.cuh"
 
 int main() {
+
+	cudaSetDevice(0);
 
 	int M, N, K;
 	M = N = 3;
 	K = 1;
 
-	double* A, * B, * C;
+	double *A, *B, *C , *d_A, *d_B, *d_C;
 
 	A = (double*)malloc(sizeof(double) * M * N);
 	B = (double*)malloc(sizeof(double) * N * K);
 	C = (double*)malloc(sizeof(double) * M * K);
+
+	cudaMalloc(&d_A, sizeof(double) * M * N);
+	cudaMalloc(&d_B, sizeof(double) * N * K);
+	cudaMalloc(&d_C, sizeof(double) * M * K);
+
 
 	for (int i = 0; i < M * N; i++) {
 		A[i] = 1.;
@@ -24,7 +32,24 @@ int main() {
 		C[i] = 1.;
 	}
 
-	Kernel(M, N, K, A, B, C);
+	cudaMemcpy(d_A, A, sizeof(double) * M * N, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, B, sizeof(double) * N * K, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_C, C, sizeof(double) * M * K, cudaMemcpyHostToDevice);
+
+
+	cudaDeviceProp devProp;
+	
+	cudaGetDeviceProperties(&devProp, 0);
+	int nThreads = (int)(devProp.maxThreadsPerBlock / 4);
+	//최대로 쓰나 4개를 쓰나 비슷하다.
+	int nBlocks = 65535;
+	//관행 
+
+	Kernel<<<nBlocks, nThreads>>> (M, N, K, d_A, d_B, d_C);
+
+	cudaMemcpy(C, d_C, sizeof(double) * M * K, cudaMemcpyDeviceToHost);
+
+
 	
 	int ind = 0;
 
@@ -39,5 +64,14 @@ int main() {
 			break;
 		}
 	}
+
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+
+	free(A);
+	free(B);
+	free(C);
+
 	return 0;
 }
